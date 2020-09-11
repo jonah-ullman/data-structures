fd3017e1f465a396425fc9abb0d617b1f9a100c5
Jonah Ullman <jonahmullman@gmail.com>
2020-09-10 20:10:53 -0500

    sll - added unshift

--- sll.js
+++ sll.js
@@ -59,4 +59,17 @@ class SinglyLinkedList {
       return temp;
     }
   }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
