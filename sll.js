760c3af48d2d09a4dce3f8aa02425660fa76d505
Jonah Ullman <jonahmullman@gmail.com>
2020-09-10 19:42:40 -0500

    sll - added push

--- sll.js
+++ sll.js
@@ -4,3 +4,22 @@ class Node {
     this.next = null;
   }
 }
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+}
